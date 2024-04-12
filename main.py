3e9db49aed125e8c92be898d478c373d870ae3eb
Rafael Lara <92409857+faellm@users.noreply.github.com>
2024-04-12 20:22:07 -0300

    convert str to int

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
-# Importando as bibliotecas necessárias
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 # Carregando o conjunto de dados a partir do arquivo CSV
 df = pd.read_csv("./treinamento_alunos.csv")
 
+# Convertendo a coluna alvo em formato numérico
+df['Original_NU_NOTA_REDACAO'] = pd.to_numeric(df['Original_NU_NOTA_REDACAO'])
+
+# Separar features numéricas e categóricas
+numeric_features = df.select_dtypes(include=['float64', 'int64'])
+categorical_features = df.select_dtypes(include=['object'])
+
+# Aplicar normalização às features numéricas
+scaler = StandardScaler()
+numeric_features_scaled = scaler.fit_transform(numeric_features)
+
+# Aplicar codificação one-hot às features categóricas
+encoder = OneHotEncoder()
+categorical_features_encoded = encoder.fit_transform(categorical_features)
+
+# Juntar as features numéricas e categóricas transformadas
+X = pd.concat([pd.DataFrame(numeric_features_scaled), pd.DataFrame(categorical_features_encoded.toarray())], axis=1)
+
 # Separando os dados em features (X) e target (y)
-X = df.drop(columns=['Original_NU_NOTA_REDACAO'])  # previsões
 y = df['Original_NU_NOTA_REDACAO']  # variável que queremos prever
 
 # Dividindo os dados em conjuntos de treinamento e teste
-
-# (train_test_split) = nos ajuda a dividir nossos dados em conjuntos de treinamento e teste
-# (X_train): Conjunto de features para treinamento.
-# (X_test:) Conjunto de features para teste.
-# (y_train): Conjunto de targets correspondentes ao treinamento.
-# (y_test:) Conjunto de targets correspondentes ao teste.
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=25)
 
 # Criando e treinando o modelo Gradient Boosting
